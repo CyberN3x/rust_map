79e2df3e84a336ab491de806e80599b8e47f9b1f
nex <nexaura@proton.me>
2024-01-20 08:50:28 -0500

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,12 @@ struct Args{
     #[clap(default_value_t = 1000)]
     ///The port number to end the scan on. If left blank will default to 1000
     end_port: u16,
+    #[clap(short = 's', long = "service-check")]
+    service_check: bool,
 }
 
 struct OpenPort{
-    port_info: Vec<(u16, Option<String>)>,
+    port_info: Vec<(u16, bool, Option<String>)>,
 }
 
 async fn web_request(host: Ipv4Addr, port: u16) -> Result<String>{
@@ -59,52 +61,71 @@ async fn ssh_info(host: Ipv4Addr, port: u16) ->Result<String>{
     Ok(ssh_version)
 }
 
-
-#[tokio::main]
-async fn main() {
-    let args = Args::parse();
-    let mut oport = OpenPort{
-        port_info: Vec::new(),
-    };
-    let start = Instant::now();
-
-    println!("Scaning Host: {} Port Range: {}-{}", args.host, args.start_port, args.end_port);
-    println!("Scaning Host: {}", args.host);
-    println!("Port Range: {} - {} ", args.start_port, args.end_port);
-    for port in args.start_port..=args.end_port {
-       match TcpStream::connect(format!("{}:{}", args.host, port)).await{
-            Ok(_stream) => {
-               let banner = if port == 80 || port == 443{
-                    web_request(args.host, port).await.ok()                  
-                }
-                else if port == 22{
-                    ssh_info(args.host, port).await.ok()
-                }
-                else {
+async fn scan_port_chunk(host: Ipv4Addr, ports: Vec<u16>, banner:bool) ->Vec <(u16, bool, Option<String>)>{
+    let mut results = Vec::new();
+    for port in ports {
+        match TcpStream::connect(format!("{}:{}", host,port)).await{
+            Ok(_) =>{
+                let infobanner = if (port == 80 || port == 443) && banner{
+                     web_request(host, port).await.ok()
+                }else if port == 22 && banner {
+                    ssh_info(host, port).await.ok()
+                }else{
                     None
                 };
-
-                oport.port_info.push((port, banner));
+                results.push((port, true, infobanner))
             }
-            Err(_e) => {
+            Err(_) => results.push((port, false, None)),
+        }
+    }
+    results
+}
+
+#[tokio::main(flavor ="multi_thread", worker_threads = 5)]
+async fn main() {
+    let args = Args::parse();
+    let start = Instant::now();
+    let host = args.host;
+    let sport = args.start_port;
+    let eport = args.end_port;
+    let check_banner = args.service_check;
+    let total_ports : Vec<_> = (sport..=eport).collect();
+    let chunk_size = total_ports.len() /5;
+
+    println!("Scaning Host: {} Port Range: {}-{}", host, sport, eport);
+    let mut tasks = Vec::new();
+    for port_chunk in total_ports.chunks(chunk_size) {
+        let host = host.clone();
+        let chunk: Vec<_> = port_chunk.to_vec();
+        let task = tokio::spawn(async move{
+            scan_port_chunk(host, chunk, check_banner).await
+        });
+        tasks.push(task);
+    }
+
+    let mut oport = OpenPort{ port_info: Vec::new() };
+    for task in tasks{
+        let result = task.await.unwrap();
+        oport.port_info.extend(result);
+        
+    }
+
+    for(port, is_open, banner) in &oport.port_info{
+        if *is_open {
+            if let Some(b) = banner{
+                println!("Open Port {}", port);
+                println!("Banner {}", b);
+            }else{
+                println!("Open Port: {}", port);
             }
         }
     }
-
     let stime = start.elapsed();
     let min = stime.as_secs() / 60;
     let sec = stime.as_secs() % 60;
     let millisec = stime.as_millis();
     let ftime = format!("{}:{}.{}", min, sec, millisec);
 
-    for (port, banner) in &oport.port_info{
-        if let Some(b) = banner{
-            println!("Open Port {}", port);
-            println!("Banner {}", b);
-        }else{
-            println!("Open Port: {}", port);
-        }
-    }
     println!("Total scan time {}", ftime);
     
 }
